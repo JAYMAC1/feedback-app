65d5454da614647325559bc9c7d4828507b9f3b1
James McNeil <mcneil_james@hotmail.com>
2022-01-25 13:44:29 +0000

    removed App.css and updated Header and App.js for project examples

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/Header'
 function App() {
   return (
     <>
-      <Header text='hello world' />
+      <Header text='Feedback UI' />
       <div className='container'>
         <h1>My App</h1>
       </div>
