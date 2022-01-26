adb66d2035577069fe3bb71a02fccc82cbe8aa53
James McNeil <mcneil_james@hotmail.com>
2022-01-26 20:15:02 +0000

    removed handleClick function

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,10 +1,4 @@
 function FeedbackItem({ item }) {
-  const handleClick = (e) => {
-    e.preventDefault()
-    setRating((prev) => {
-      return prev + 1
-    })
-  }
   return (
     <div className='card'>
       <div className='num-display'>{item.rating}</div>
