4fe3264a88f98f24a22b0c13830d86d043814960
James McNeil <mcneil_james@hotmail.com>
2022-01-26 20:13:23 +0000

    removed state to Feedback list

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,18 +1,14 @@
-import React, { useState } from 'react'
-
-function FeedbackItem() {
-  const [rating, setRating] = useState(7)
-  const [text, setText] = useState('This is an example of a feedback item.')
-
+function FeedbackItem({ item }) {
   const handleClick = (e) => {
     e.preventDefault()
-    setRating(10)
+    setRating((prev) => {
+      return prev + 1
+    })
   }
   return (
     <div className='card'>
-      <div className='num-display'>{rating}</div>
-      <div className='text-display'>{text}</div>
-      <button onClick={handleClick}></button>
+      <div className='num-display'>{item.rating}</div>
+      <div className='text-display'>{item.text}</div>
     </div>
   )
 }
