7ac376e06476072d175c6d611f545cb396190796
James McNeil <mcneil_james@hotmail.com>
2022-01-28 13:07:16 +0000

    handleChange function on Button to set state for isDisabled and Message with conditions

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,23 +1,45 @@
 import React, { useState } from 'react'
+import Button from './shared/Button'
 import { Card } from './shared/Card'
 
 const FeedbackForm = () => {
   const [text, setText] = useState('')
+  const [btnDisabled, setBtnDisabled] = useState(true)
+  const [message, setMessage] = useState('')
+
+  const handleTextChange = (e) => {
+    e.preventDefault()
+    if (text === '') {
+      setBtnDisabled(true)
+      setMessage(null)
+    } else if (text !== '' && text.trim().length <= 10) {
+      setBtnDisabled(true)
+      setMessage('Text must be at least 10 characters')
+    } else {
+      setMessage(null)
+      setBtnDisabled(false)
+    }
+    setText(e.target.value)
+  }
+
   return (
     <Card>
       <form>
         <h2>How would you rate your service with us? </h2>
+        {/* @todo - rating select component */}
+        <div className='input-group'>
+          <input
+            type='text'
+            placeholder='write a review'
+            onChange={handleTextChange}
+            value={text}
+          />
+          <Button type='submit' isDisabled={btnDisabled}>
+            Send
+          </Button>
+        </div>
+        {message && <div className='message'>{message}</div>}
       </form>
-      {/* @todo - rating select component */}
-      <div className='input-group'>
-        <input
-          type='text'
-          placeholder='write a review'
-          onChange={(e) => setText(e.target.value)}
-          value={text}
-        />
-        <button type='submit'>Send</button>
-      </div>
     </Card>
   )
 }
