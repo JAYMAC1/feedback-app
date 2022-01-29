f3c4ff42bbd88295ced2e954b54016e76965026c
James McNeil <mcneil_james@hotmail.com>
2022-01-29 18:04:28 +0000

    imported contexct to use deleteFeedback function in cntext

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,11 +1,21 @@
+// react components
+import { useContext } from 'react'
+
+// npm packages
 import { FaTimes } from 'react-icons/fa'
+
+// components and pages
 import { Card } from './shared/Card'
 
-function FeedbackItem({ item, handleDelete }) {
+// context and hooks
+import FeedbackContext from '../context/FeedbackContext'
+
+function FeedbackItem({ item }) {
+  const { deleteFeedback } = useContext(FeedbackContext)
   return (
     <Card>
       <div className='num-display'>{item.rating}</div>
-      <button className='close' onClick={() => handleDelete(item.id)}>
+      <button className='close' onClick={() => deleteFeedback(item.id)}>
         <FaTimes color='purple' />
       </button>
       <div className='text-display'>{item.text}</div>
