9b9523e0d4a8083d02b537d583a70c40b0c611aa
James McNeil <mcneil_james@hotmail.com>
2022-01-29 18:03:34 +0000

    moved functions add and delete to context

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,4 @@
 // react components etc
-import { useState } from 'react'
 
 // Pages and components
 import FeedbackForm from '../components/FeedbackForm'
@@ -9,29 +8,13 @@ import FeedbackStats from '../components/FeedbackStats'
 // mock data source
 import FeedbackData from '../data/FeedbackData'
 
-// packages
-import { v4 as uuidv4 } from 'uuid'
-
 const HomePage = () => {
-  const [feedback, setFeedback] = useState(FeedbackData)
-
-  const deleteFeedback = (id) => {
-    if (window.confirm('Are you sure you want to delete?')) {
-      setFeedback(feedback.filter((item) => item.id !== id))
-    }
-  }
-
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4()
-    console.log(newFeedback)
-    setFeedback([newFeedback, ...feedback])
-  }
   return (
     <>
       <div className='container'>
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+        <FeedbackForm />
+        <FeedbackStats />
+        <FeedbackList />
       </div>
     </>
   )
