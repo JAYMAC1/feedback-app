ca87a74f50e116cb950a35e54444f8d74d4b8dcb
James McNeil <mcneil_james@hotmail.com>
2022-01-29 14:41:28 +0000

    initial creation of AboutIcon

---
+++ src/components/AboutIconLink.jsx
@@ -0,0 +1,13 @@
+import { FaQuestion } from 'react-icons/fa'
+
+import React from 'react'
+
+const AboutIconLink = () => {
+  return (
+    <div className='about-link'>
+      <FaQuestion size={30} />
+    </div>
+  )
+}
+
+export default AboutIconLink
