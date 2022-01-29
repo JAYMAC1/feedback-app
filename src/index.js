3e1017826c4ccc8138adaace18b9efef326cd856
James McNeil <mcneil_james@hotmail.com>
2022-01-29 16:32:38 +0000

    wrapped App with context provider

--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
+// context
+import { FeedbackProvider } from './context/FeedbackContext'
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <FeedbackProvider>
+      <App />
+    </FeedbackProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
