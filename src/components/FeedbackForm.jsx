5a566333e7a77e3dc201e48b930414cdb4bb76ae
James McNeil <mcneil_james@hotmail.com>
2022-01-29 13:14:47 +0000

    subitting form and passing as props back to App to update feedback state

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
+import RatingSelect from './RatingSelect'
 import Button from './shared/Button'
 import { Card } from './shared/Card'
 
-const FeedbackForm = () => {
+const FeedbackForm = ({ handleAdd }) => {
   const [text, setText] = useState('')
+  const [rating, setRating] = useState(10)
   const [btnDisabled, setBtnDisabled] = useState(true)
   const [message, setMessage] = useState('')
 
@@ -22,11 +24,27 @@ const FeedbackForm = () => {
     setText(e.target.value)
   }
 
+  const select = (rating) => {
+    setRating(rating)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (text.trim().length > 10) {
+      const newFeedback = {
+        rating: rating,
+        text: text,
+      }
+      handleAdd(newFeedback)
+      // setText('')
+    }
+  }
+
   return (
     <Card>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>How would you rate your service with us? </h2>
-        {/* @todo - rating select component */}
+        <RatingSelect select={select} />
         <div className='input-group'>
           <input
             type='text'
