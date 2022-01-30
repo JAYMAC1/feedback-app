20390b5d35c5c1aa0789b335a771c6b30880eb53
James McNeil <mcneil_james@hotmail.com>
2022-01-30 15:17:23 +0000

    removed hard coded mock data and added fetch function

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,30 +1,28 @@
-import { createContext, useState } from 'react'
+import { createContext, useState, useEffect } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      rating: 10,
-      text: 'This is review item 1.',
-    },
-    {
-      id: 2,
-      rating: 5,
-      text: 'This is review item 2.',
-    },
-    {
-      id: 3,
-      rating: 10,
-      text: 'This is review item 3.',
-    },
-  ])
-
+  const [isPending, setIsPending] = useState(true)
+  const [feedback, setFeedback] = useState([])
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false,
   })
+
+  useEffect(() => {
+    fetchFeedback()
+  }, [])
+
+  // fetch feedback
+  const fetchFeedback = async () => {
+    const res = await fetch(
+      'http://localhost:5000/feedback?_sort=id&_order=desc'
+    )
+    const data = await res.json()
+    setFeedback(data)
+    setIsPending(false)
+  }
 
   // Add feedback to the array/collection
   const addFeedback = (newFeedback) => {
@@ -68,6 +66,7 @@ export const FeedbackProvider = ({ children }) => {
         addFeedback,
         editFeedback,
         updateFeedback,
+        isPending,
       }}>
       {children}
     </FeedbackContext.Provider>
