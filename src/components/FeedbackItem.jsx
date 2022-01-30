1e385f13506076b9875c1eb755b6b6402774c354
James McNeil <mcneil_james@hotmail.com>
2022-01-30 09:37:28 +0000

    imported FAEdit and used editFeedback from context

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -2,7 +2,7 @@
 import { useContext } from 'react'
 
 // npm packages
-import { FaTimes } from 'react-icons/fa'
+import { FaTimes, FaEdit } from 'react-icons/fa'
 
 // components and pages
 import { Card } from './shared/Card'
@@ -11,12 +11,15 @@ import { Card } from './shared/Card'
 import FeedbackContext from '../context/FeedbackContext'
 
 function FeedbackItem({ item }) {
-  const { deleteFeedback } = useContext(FeedbackContext)
+  const { deleteFeedback, editFeedback } = useContext(FeedbackContext)
   return (
     <Card>
       <div className='num-display'>{item.rating}</div>
       <button className='close' onClick={() => deleteFeedback(item.id)}>
         <FaTimes color='purple' />
+      </button>
+      <button className='edit' onClick={() => editFeedback(item)}>
+        <FaEdit color='purple' />
       </button>
       <div className='text-display'>{item.text}</div>
     </Card>
